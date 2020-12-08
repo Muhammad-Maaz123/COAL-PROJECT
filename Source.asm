069645d2b5998377e2c082319962d756dd9e17f1
talalahmed109 <33374784+talalahmed109@users.noreply.github.com>
2020-12-08 18:34:35 +0500

    Add files via upload

---
+++ Source.asm
@@ -0,0 +1,21 @@
+; Author: Abuzar Ghafari 
+; Program Name: exp9 
+ ; Program Description: TYPE Operator example 
+ ; Date 
+ 
+ INCLUDE Irvine32.inc 
+ 
+ .data 
+ bvar byte ? ; 1 byte variable 
+ wvar word ? ; 2 byte variable 
+ dvar dword ? ; 4 byte variable 
+ qvar qword ? ; 8 byte variable 
+ .code 
+ main PROC 
+ 
+ mov eax, TYPE bvar ; eax = 1, size of bvar in bytes 17 mov ebx, TYPE wvar ; ebx = 2, size of wvar in bytes 18 mov ecx, TYPE dvar ; ecx = 4, size of dvar in bytes 19 mov edx, TYPE qvar ; eax = 8, size of qvar in bytes 
+ 
+ call WaitMsg ; Press any key to continue... 
+ exit 
+ main ENDP 
+ END main 
